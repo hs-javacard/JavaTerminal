d8aee956796564f11edf79179f4883047700266a
MartijnvDijk <martijn.van.dijk@live.nl>
2019-05-23 12:23:40 +0200

    Protocol update 1

--- src/terminal/Protocol.java
+++ src/terminal/Protocol.java
@@ -2,7 +2,11 @@ package terminal;
 
 import javacard.framework.*;
 
-public class Protocol{
+public class Protocol  implements ISO7816{
+
+    private short cardNumber;
+    private short cardState;
+    private short pin;
 
     public void Protocol(){
 
@@ -12,8 +16,28 @@ public class Protocol{
 
     }
 
-    public void authentication(){
-        //TODO
+    public void authentication(APDU apdu){
+        byte[] buffer = apdu.getBuffer();
+        byte ins = buffer[OFFSET_INS];
+
+        switch (ins) {
+            case 0: // request card number
+                cardNumber = requestCardNumber(apdu);
+                break;
+            case 1: // check soft limit
+                requestPIN(apdu);
+                break;
+            case 2: // check pin
+                checkPin(apdu);
+                break;
+            default:
+                ISOException.throwIt(SW_INCORRECT_P1P2);
+                break;
+        }
+    }
+
+    private void sendResponse(APDU apdu, short length) {
+        apdu.setOutgoingAndSend((short) 0, length);
     }
 
     public void withdrawal(){
@@ -32,5 +56,51 @@ public class Protocol{
         //TODO
     }
 
+    private void checkPin(APDU apdu) {
+    }
 
+    private void requestPIN(APDU apdu) {
+        pin = Util.getShort(apdu.getBuffer(), (short) 1);
+
+
+    }
+
+    private short requestCardNumber(APDU apdu) {
+        byte[] buffer = apdu.getBuffer();
+        cardNumber = Util.getShort(buffer, (short) 1);
+
+        //TODO Terminal search for CN and related BAN in bank system
+        // to combine it to find ID of the card, and records
+        // request by the card in the logs.
+
+        if(verifyCardStatus(apdu, cardNumber)){
+            return cardNumber;
+        }
+        else {
+            ISOException.throwIt(SW_CONDITIONS_NOT_SATISFIED);
+        }
+
+        return 0;
+    }
+
+    private boolean verifyCardStatus(APDU apdu, short cardNumber) {
+        //TODO Terminal verifies if the card is not in
+        // ‘Locked’ state (Blacklisted or Blocked)
+        byte[] buffer = apdu.getBuffer();
+        cardState = 0;
+
+        if(cardNumber == 0) {
+            cardState = Util.getShort(buffer, (short) 1);
+
+            if (cardState == Short.parseShort("ready")) {
+                //TODO Continue communication
+                return true;
+            } else {
+                //TODO Abort communication, show abort message on terminal screen.
+                // Terminal logs attempt of locked card.
+                return false;
+            }
+        }
+        return false;
+    }
 }
